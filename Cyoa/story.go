0d7980da24496c384abf09e5f6243c598cc5b5cd
Benjamin Daniel <benjamindaniel706@gmail.com>
2019-10-13 12:01:27 +0100

    styled the template

--- Cyoa/story.go
+++ Cyoa/story.go
@@ -17,29 +17,65 @@ var tpl *template.Template
 
 var defaultHandlerTemplate = `
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Choose your own Adventure</title>
-</head>
-
-<body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-    <p>{{.}}</p>
-    {{end}}
-    <ul>
-        {{range .Options}}
-            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-        {{end}}
-    </ul>
-</body>
-
-</html>
-`
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p>{{.}}</p>
+      {{end}}
+      <ul>
+      {{range .Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+      </ul>
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FCF6FC;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #797;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: underline;
+        color: #555;
+      }
+      a:active,
+      a:hover {
+        color: #222;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </body>
+</html>`
 
 func NewHandler(s Story) http.Handler {
 	return handler{s}
